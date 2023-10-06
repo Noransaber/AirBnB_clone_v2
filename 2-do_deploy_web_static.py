54e1e97719512889fd9d03eedbd803b07246e81c
Noransaber <noransaber685@gmail.com>
2023-10-06 14:22:31 +0300

    Updates

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -35,16 +35,29 @@ def do_deploy(archive_path):
         fn_with_ext = os.path.basename(archive_path)
         fn_no_ext, ext = os.path.splitext(fn_with_ext)
         dpath = "/data/web_static/releases/"
-        put(archive_path, "/tmp/")
-        run("rm -rf {}{}/".format(dpath, fn_no_ext))
-        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
-        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
-        run("rm /tmp/{}".format(fn_with_ext))
+        tmp_archive_path = "/tmp/" + fn_with_ext
+
+        # Upload the archive to the server
+        put(archive_path, tmp_archive_path)
+
+        # Create a new directory for the release
+        run("mkdir -p {0}{1}".format(dpath, fn_no_ext))
+
+        # Unpack the archive into the new directory
+        run("tar -xzf {0} -C {1}{2}".format(tmp_archive_path, dpath, fn_no_ext))
+
+        # Delete the temporary archive file
+        run("rm {0}".format(tmp_archive_path))
+
+        # Move the contents to the current directory
         run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
-        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
+
+        # Remove the old symbolic link and create a new one
         run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
+        run("ln -s {0}{1}/ /data/web_static/current".format(dpath, fn_no_ext))
+
         print("New version deployed!")
         return True
-    except Exception:
+    except Exception as e:
+        print("Deployment error:", str(e))
         return False
