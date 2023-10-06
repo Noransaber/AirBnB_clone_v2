089ca42acc9f83b21d35b1ed62a3ec14a15f5e25
root <noransaber685@gmail.com>
2023-10-06 10:58:32 +0000

    Updates

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,50 +1,54 @@
-#!/usr/bin/python3
-""" module doc
-"""
-from fabric.api import task, local, env, put, run
+from fabric.api import env, put, run
 from datetime import datetime
-import os
+from os.path import exists, isdir
 
-env.hosts = ['18.207.1.87', '52.206.189.175']
-
-
-@task
-def do_pack():
-    """ method doc
-        sudo fab -f 1-pack_web_static.py do_pack
-    """
-    formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
-    mkdir = "mkdir -p versions"
-    path = "versions/web_static_{}.tgz".format(formatted_dt)
-    print("Packing web_static to {}".format(path))
-    if local("{} && tar -cvzf {} web_static".format(mkdir, path)).succeeded:
-        return path
-    return None
-
-
-@task
 def do_deploy(archive_path):
-    """ method doc
-        fab -f 2-do_deploy_web_static.py do_deploy:
-        archive_path=versions/web_static_20231004201306.tgz
-        -i ~/.ssh/id_rsa -u ubuntu
-    """
-    try:
-        if not os.path.exists(archive_path):
-            return False
-        fn_with_ext = os.path.basename(archive_path)
-        fn_no_ext, ext = os.path.splitext(fn_with_ext)
-        dpath = "/data/web_static/releases/"
-        put(archive_path, "/tmp/")
-        run("rm -rf {}{}/".format(dpath, fn_no_ext))
-        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
-        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
-        run("rm /tmp/{}".format(fn_with_ext))
-        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
-        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
-        print("New version deployed!")
-        return True
-    except Exception:
-        return False
+  """
+  Deploys an archive to the web servers.
+
+  Args:
+    archive_path: The path to the archive file.
+
+  Returns:
+    True if the deployment was successful, False otherwise.
+  """
+
+  if not exists(archive_path):
+    return False
+
+  # Upload the archive to the /tmp/ directory of the web server
+  put(archive_path, '/tmp/')
+
+  # Uncompress the archive to the folder /data/web_static/releases/<archive filename without extension> on the web server
+  file_n = archive_path.split("/")[-1]
+  no_ext = file_n.split(".")[0]
+  path = "/data/web_static/releases/"
+  run('mkdir -p {}{}/'.format(path, no_ext))
+  run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+
+  # Delete the archive from the web server
+  run('rm /tmp/{}'.format(file_n))
+
+  # Delete the symbolic link /data/web_static/current from the web server
+  run('rm -rf /data/web_static/current')
+
+  # Create a new the symbolic link /data/web_static/current on the web server, linked to the new version of your code (/data/web_static/releases/<archive filename without extension>)
+  run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+
+  return True
+
+# Set the hosts to deploy to
+env.hosts = ['xx-web-01', 'xx-web-02']
+
+# Deploy the archive
+def deploy():
+  archive_path = 'versions/web_static_{}.tgz'.format(datetime.now().strftime("%Y%m%d%H%M%S"))
+
+  if not do_deploy(archive_path):
+    print('Deployment failed.')
+    return
+
+  print('Deployment successful.')
+
+if __name__ == '__main__':
+  deploy()
