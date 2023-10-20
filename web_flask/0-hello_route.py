824a7dbb9a11694a204fd44edb7f3872c8dc9948
Noransaber <noransaber685@gmail.com>
2023-10-20 11:11:38 +0300

    Updates

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+"""Starts a Flask web application:
+/: display “Hello HBNB!”
+"""
 from flask import Flask
 
 # Starting the application
 app = Flask(__name__)
 
+
 # Routing to the path '/'
-
-
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Return Hello HBNB"""
     return "Hello HBNB!"
 
 
