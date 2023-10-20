3677f665cc49d6fa61d8c12869bfc9ddc44b2db1
Noransaber <noransaber685@gmail.com>
2023-10-20 11:07:13 +0300

    adding Doc + remoting spaces

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" starts a Flask web application:
+"""Starts a Flask web application:
     /: display “Hello HBNB!”
     /hbnb: display “HBNB”
 """
@@ -7,12 +7,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Return hello_hbnb"""
     return 'Hello HBNB!'
+
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Return HBNB"""
     return 'HBNB'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
