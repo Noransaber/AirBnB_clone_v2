ba58e950d215ed5007e3b21308a2e9b029a5efb9
Noransaber <noransaber685@gmail.com>
2023-10-22 16:00:51 +0300

    Fixing pycodestyle

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,24 +4,28 @@ Web Flask Application
 """
 
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
+
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """
-	Displaying a HTML page with states orderd alphabetically
-	"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    Display a HTML page with states ordered alphabetically.
+    """
+    states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Storage Close"""
+    """
+    Close the database connection at the end of the application context.
+    """
     storage.close()
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
